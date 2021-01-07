0c4391e084ccd84d03d7ee9be1991b22663507ad
edward.z10 <edward.ziyangulov@gmail.com>
2021-01-07 18:24:36 +0300

    lesson 2

---
+++ app/src/main/java/com/jonnyb/swoosh/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package com.jonnyb.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
